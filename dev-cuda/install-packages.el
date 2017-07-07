97ae7c3002e2924def93715a1c5aa0a14a2e506d
OJIMA Ryoji <rojima1@gmail.com>
2017-07-07 10:13:36 +0900

    python-dev-cuda is added.

--- dev-cuda/install-packages.el
+++ dev-cuda/install-packages.el
@@ -1,1 +1,21 @@
-../dev/install-packages.el
+(defvar packages
+  '(cider
+    clj-refactor
+    clojure-mode
+    elpy
+    helm
+    markdown-mode))
+
+(defun install-packages ()
+  (package-refresh-contents)
+  (dolist (package packages)
+    (unless (package-installed-p package)
+      (package-install package))))
+
+(defun init-package ()
+  (require 'package)
+  (add-to-list 'package-archives '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/"))
+  (package-initialize))
+
+(init-package)
+(install-packages)
